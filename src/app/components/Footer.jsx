883557a4b48ae6c036974c8906d97c180d1e609c
Gautam Sodhi <129111379+Iam-Sodhi@users.noreply.github.com>
2023-07-24 04:19:37 +0530

    2 video completed

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,13 @@
+
+function Footer() {
+
+    const currYear= new Date().getFullYear();
+    return (
+        <footer>
+            <p> Copyright ©️ {currYear}</p>
+        </footer>
+    )
+
+
+}
+export default Footer;
